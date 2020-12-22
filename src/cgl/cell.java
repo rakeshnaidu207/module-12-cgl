c9971ea4dfcf6b1f2abbf0cb3142e57897836572
Y.RAKESH NAIDU <Y.RAKESH NAIDU@DESKTOP-ATVHHE8>
2020-12-22 22:13:54 +0530

    rakesh first copy

--- src/cgl/cell.java
+++ src/cgl/cell.java
@@ -5,9 +5,9 @@ public class cell {
 	    // Set the size for each cell
 	    int width = 10 ;
 	    int height = 10;
-
-	    public constructor (context, gridX, gridY)
-	    {
+            
+	    public void constructor (context, gridX, gridY){
+	    
 	        this.context = context;
 
 	        // Store the position of this cell in the grid
@@ -18,7 +18,7 @@ public class cell {
 	        this.alive = Math.random() > 0.5;
 	    }
 
-	    public draw() {
+	    public void draw() {
 	        // Draw a square, let the state determine the color
 	        this.context.fillStyle = this.alive?'#ff8080':'#303030';
 	        this.context.fillRect(this.gridX * Cell.width, this.gridY * Cell.height, Cell.width, Cell.height);
